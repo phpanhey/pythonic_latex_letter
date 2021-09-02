c5141cfc20cffbf865850e1d3c0da6e4f587693e
Philipp Panhey <philipp.panhey@bo-bremen.de>
2021-09-02 11:43:18 +0200

    read config file

--- pythonic_latex_letter.py
+++ pythonic_latex_letter.py
@@ -1,5 +1,18 @@
+import argparse
+import json
+
 def main():
-    print("hello world")
+    config = get_config()
+
+def get_config():
+ config_file_name = parse_args()["config"] 
+ with open(config_file_name) as config_file:
+    return json.load(config_file)
+
+def parse_args():
+    parser = argparse.ArgumentParser()    
+    parser.add_argument("--config", default="config.json")    
+    return vars(parser.parse_args())
     
 if __name__ == "__main__":
     main()
